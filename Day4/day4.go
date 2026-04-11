954bc6497a521de4b416c24ee9c22b24d1efefb9
Andrés Núñez <nunezlopez.ja@gmail.com>
2026-04-11 17:13:27 -0600

    Phew that was fun but ass at the end. Day4 complete

--- Day4/day4.go
+++ Day4/day4.go
@@ -6,12 +6,16 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
+	"unicode"
 )
 
 var RequiredFields = []string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
+
 var ValidHCLLetters = []string{"a", "b", "c", "d", "e", "f"}
+var ValidECL = []string{"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
 
 type Passport struct {
 	fields map[string]string
@@ -29,8 +33,7 @@ func (p Passport) New(text string) Passport {
 		fields_map[key] = value
 	}
 
-	valid := true
-	passport := Passport{fields: fields_map, valid: valid}
+	passport := Passport{fields: fields_map, valid: true}
 
 	// Checking if valid
 	// So, for part 2 is not enough to check if all fields (except for that one) are present
@@ -41,7 +44,7 @@ func (p Passport) New(text string) Passport {
 			passport.valid = false
 			break
 		}
-		switch got {
+		switch field {
 		case "byr":
 			if len(got) != 4 {
 				passport.valid = false
@@ -50,11 +53,11 @@ func (p Passport) New(text string) Passport {
 			year, err := strconv.Atoi(got)
 			if err != nil {
 				fmt.Println("Error with year:", year, err.Error())
-				valid = false
+				passport.valid = false
 				return passport
 			}
 			if year < 1920 || year > 2002 {
-				valid = false
+				passport.valid = false
 			}
 		case "iyr":
 			if len(got) != 4 {
@@ -64,11 +67,11 @@ func (p Passport) New(text string) Passport {
 			year, err := strconv.Atoi(got)
 			if err != nil {
 				fmt.Println("Error with year:", year, err.Error())
-				valid = false
+				passport.valid = false
 				return passport
 			}
 			if year < 2010 || year > 2020 {
-				valid = false
+				passport.valid = false
 			}
 		case "eyr":
 			if len(got) != 4 {
@@ -78,27 +81,30 @@ func (p Passport) New(text string) Passport {
 			year, err := strconv.Atoi(got)
 			if err != nil {
 				fmt.Println("Error with year:", year, err.Error())
-				valid = false
+				passport.valid = false
 				return passport
 			}
 			if year < 2020 || year > 2030 {
-				valid = false
+				passport.valid = false
 				return passport
 			}
 		case "hgt":
+			if strings.Contains(got, "cm") && strings.Contains(got, "in") {
+				fmt.Println("Both:", got)
+			}
 			if !strings.Contains(got, "cm") && !strings.Contains(got, "in") {
-				valid = false
+				passport.valid = false
 				return passport
 			}
 			if strings.Contains(got, "cm") {
 				height, err := strconv.Atoi(strings.TrimSuffix(got, "cm"))
 				if err != nil {
 					fmt.Println("Error with height:", got, err.Error())
-					valid = false
+					passport.valid = false
 					return passport
 				}
 				if height < 150 || height > 193 {
-					valid = false
+					passport.valid = false
 					return passport
 				}
 			}
@@ -106,26 +112,57 @@ func (p Passport) New(text string) Passport {
 				height, err := strconv.Atoi(strings.TrimSuffix(got, "in"))
 				if err != nil {
 					fmt.Println("Error with height:", got, err.Error())
-					valid = false
+					passport.valid = false
 					return passport
 				}
 				if height < 59 || height > 76 {
-					valid = false
+					passport.valid = false
 					return passport
 				}
 			}
 		case "hcl":
-			if !strings.Contains(got, "#") {
-				valid = false
+			if got[0] != '#' {
+				passport.valid = false
 				return passport
 			}
 			characters := strings.TrimPrefix(got, "#")
 			if len(characters) != 6 {
-				valid = false
+				passport.valid = false
 				return passport
 			}
-			for _, c := range characters {
-
+			// I suspect I have to check here if all characters that are letters DO NOT go past 'f', but for now I'm at a loss
+			// We'll com back to this later
+			for _, i := range characters {
+				if !unicode.IsDigit(i) && !(i >= 'a' && i <= 'f') {
+					passport.valid = false
+					return passport
+				}
+			}
+		case "ecl":
+			if len(got) != 3 {
+				fmt.Println("Invalid eye color: ", got)
+				passport.valid = false
+				return passport
+			}
+			if slices.Contains(ValidECL, got) {
+				continue
+			} else {
+				passport.valid = false
+				return passport
+			}
+		case "pid":
+			if len(got) != 9 {
+				fmt.Println("Invalid Passport ID:", got)
+				passport.valid = false
+				return passport
+			}
+			// pid_number := strconv.Atoi(got)
+			for _, i := range got {
+				if !unicode.IsDigit(i) {
+					fmt.Println("Invalid character in PID:", i)
+					passport.valid = false
+					return passport
+				}
 			}
 		}
 	}
