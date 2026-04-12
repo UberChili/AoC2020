48996d36565fc5cb13b8247538bba588434612f3
Andres Nunez <nunezlopez.ja@gmail.com>
2026-04-12 14:41:42 -0600

    Finished Day 5 Part 2 and cleaned up a little

--- Day5/day5.go
+++ Day5/day5.go
@@ -12,20 +12,29 @@ func main() {
 	fmt.Println("Hello there!")
 
 	// example := "FBFBBFFRLR"
-
 	seats := read_lines("input.txt")
+
+	// Part 1 requires us to calculate seat ID's and then
+	// just find the highest seat ID:
 	seats_ids := make([]int, 0)
 	for _, seat := range seats {
-		seats_ids = append(seats_ids, sliceToDecimal(binary_search(seat)))
+		seats_ids = append(seats_ids, sliceToDecimal(parseSeat(seat)))
 	}
 	highest := slices.Max(seats_ids)
 	fmt.Println("Highest seat ID:", highest)
 
-	// number := binary_search(example)
-	// number_in_decimal := sliceToDecimal(number)
-	// fmt.Println(number_in_decimal)
+	// Part 2 requires us to find the missing Seat ID in the entire sequence
+	// So we need the sorted list of Seat ID's first
+	slices.Sort(seats_ids)
+	for i := 1; i < len(seats_ids); i++ {
+		if (seats_ids[i] - seats_ids[i-1]) != 1 {
+			fmt.Println("Part 2 Seat:", seats_ids[i-1]+1)
+			break
+		}
+	}
 }
 
+// Copied this, tbh
 func sliceToDecimal(bits []int) int {
 	decimal := 0
 	for _, bit := range bits {
@@ -35,7 +44,9 @@ func sliceToDecimal(bits []int) int {
 	return decimal
 }
 
-func binary_search(seat string) []int {
+// Previously called binary_search, but we aren't actually searching nor are we actually using
+// Binary search, so we'll rename it to something more descriptive
+func parseSeat(seat string) []int {
 	if seat == "" {
 		fmt.Println("Error with seat. Can't be empty:", seat)
 		return nil
