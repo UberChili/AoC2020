19842d66b2e96527f33c53e979ae318861b0128c
Andrés Núñez <nunezlopez.ja@gmail.com>
2026-04-21 17:31:12 -0600

    Commented code out for part 1. Started work on part 2
    Started a new file for the biggest chunks of part 2 including the
    structure definition that we need.
    commented out all the relevant code of part 1 on day7.go
    
    There has to be a better way to do this, I hope it is something that
    comes with experience. But I suppose I should not focus too much on
    that, for now

--- Day7/day7.go
+++ Day7/day7.go
@@ -9,33 +9,48 @@ import (
 )
 
 func main() {
-	lines := get_lines("input.txt")
+	lines := get_lines("example.txt")
 
 	// Our map (graph)
-	graph := make(map[string][]string)
+	// For part 1, we use a simple map
+	// graph := make(map[string][]string)
+	// For part 2, we use:
+	graph := make(map[string][]BagContent)
 
+	// Part 1. We "generate" the map:
+	// for _, line := range lines {
+	// 	parent, children := parse_bag_line(line)
+	// 	graph[parent] = children
+	// }
+
+	// Part 2. We "generate" the map:
 	for _, line := range lines {
-		parent, children := parse_bag_line(line)
+		parent, children := parse_bag_line_part2(line)
 		graph[parent] = children
 	}
 
-	// Just a printing loop to test
-	// for key, value := range graph {
-	// 	// fmt.Println(key, ":", value)
-	// 	fmt.Printf("* %s: %#v\n", key, value)
-	// }
-
-	// Loop through all bags checking if they contain shiny gold
-	bags_count := 0
-	for key := range graph {
-		if contains_gold(graph, key) {
-			bags_count += 1
+	// Just a printing loop to test if the map has correctly been formed
+	for key, values := range graph {
+		fmt.Printf("%s -> ", key)
+		for _, v := range values {
+			fmt.Printf("%d %s | ", v.amount, v.color)
 		}
+		fmt.Println()
 	}
 
-	fmt.Println("Bags that contain Shiny Gold:", bags_count)
+	// Loop through all bags checking if they contain shiny gold
+	// This is the solution for part 1
+	// bags_count := 0
+	// for key := range graph {
+	// 	if contains_gold(graph, key) {
+	// 		bags_count += 1
+	// 	}
+	// }
+
+	// fmt.Println("Bags that contain Shiny Gold:", bags_count)
 }
 
+// For part 1, we just need to go deep on every node/bag to see if it eventually contains Shiny Gold
 func contains_gold(graph map[string][]string, bag string) bool {
 	for _, child := range graph[bag] {
 		if child == "shiny gold" {
@@ -49,6 +64,8 @@ func contains_gold(graph map[string][]string, bag string) bool {
 	return false
 }
 
+// Parsing function for part 1
+// We pretty much ignore all numbers and get the bag name (node) and the bags it contains (its children)
 func parse_bag_line(text string) (string, []string) {
 	bag, rest, found := strings.Cut(text, " bags contain ")
 	if found == false {
@@ -80,6 +97,7 @@ func parse_bag_line(text string) (string, []string) {
 	return bag, child_bags
 }
 
+// Just the get lines function, this doesn't change for any part
 func get_lines(filepath string) []string {
 	// omitting file checking for brevity
 	file, err := os.Open(filepath)
---
+++ Day7/day7_part2.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	"strconv"
+	"strings"
+)
+
+// We now use a struct to keep some information abut the current bag (node)
+type BagContent struct {
+	color  string
+	amount int
+}
+
+// Parsing function for part 2
+// Now we need the numbers (the amount of 'this' bag that are contained)
+func parse_bag_line_part2(text string) (string, []BagContent) {
+	bag, rest, found := strings.Cut(text, " bags contain ")
+	if found == false {
+		return "", nil
+	}
+
+	// Extracting leave bags
+	//
+	// If line contains the word "no", then the bag contains no number of other bags
+	if strings.Contains(rest, "no") {
+		return bag, nil
+	}
+
+	// We split by commas.
+	// In the example, we have one or at most, two bags inside any bag.
+	// But in the input, we have any number of bags
+	child_bags := []BagContent{}
+	for seq := range strings.SplitSeq(rest, ",") {
+		// Remove leading whitespace that could be produced for subsequent sequences after splitting
+		seq := strings.TrimSpace(seq)
+
+		// splitting on spaces to get individual words
+		bag_name_splitted := strings.Split(seq, " ")
+
+		// Getting the amount of this bag
+		amount, err := strconv.Atoi(bag_name_splitted[0])
+		if err != nil {
+			log.Fatal("Something went wrong getting bag amount")
+		}
+
+		// formatting the actual name/color of the bag per sequence
+		bag := bag_name_splitted[1] + " " + bag_name_splitted[2]
+		child_bags = append(child_bags, BagContent{bag, amount})
+	}
+	return bag, child_bags
+}
